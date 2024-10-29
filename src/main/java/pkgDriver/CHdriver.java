e86eed7ca4fe5162dc515678fde4b6192001da51
crhen <crhender8@gmail.com>
2024-10-29 12:47:30 -0700

    Initial commit

---
+++ src/main/java/pkgDriver/CHdriver.java
@@ -0,0 +1,4 @@
+package pkgDriver;
+
+public class CHdriver {
+}
---
+++ src/main/java/pkgPingPongArray/CHPingPongArray.java
@@ -0,0 +1,44 @@
+package pkgPingPongArray;
+
+import java.util.Random;
+
+public class CHPingPongArray {
+    private final int defaultRandMin = 0;
+    private final int defaultRandMax = 1;
+    private int[][] live;
+    private int[][] next;
+    Random rand = new Random();
+    public void createArray(int rows, int cols) {
+        live = new int[rows][cols];
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                live[row][col] = rand.nextInt(defaultRandMin, defaultRandMax);
+            }
+        }
+    }
+    public void createArray(int rows, int cols, int value) {
+        live = new int[rows][cols];
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                live[row][col] = value;
+            }
+        }
+    }
+    public void createArray(int rows, int cols, int min, int max) {
+        live = new int[rows][cols];
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                live[row][col] = rand.nextInt(min, max);
+            }
+        }
+    }
+    public void setCellLive(int row, int col, int value) {
+        live[row][col] = value;
+    }
+    public void setCellNext(int row, int col, int value) {
+        next[row][col] = value;
+    }
+    public int getCellLive(int row, int col) {
+        return live[row][col];
+    }
+}
