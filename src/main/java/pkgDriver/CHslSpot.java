8a5f3109750aa5351dbf3373364323f01f16c440
crhen <crhender8@gmail.com>
2024-11-03 20:46:26 -0800

    merged code from polygon renderer

---
+++ src/main/java/pkgDriver/CHslSpot.java
@@ -0,0 +1,5 @@
+package pkgDriver;
+
+public class CHslSpot {
+    public static final int WIN_WIDTH = 1200, WIN_HEIGHT = 1200;
+}
---
+++ src/main/java/pkgKeyRead/CHkeyReader.java
@@ -0,0 +1,4 @@
+package pkgKeyRead;
+
+public class CHkeyReader {
+}
